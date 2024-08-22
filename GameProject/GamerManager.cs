1c5fd16eb723aa8ecff7de859e502f4f76e88db9
seliudev <ulucamselinn@gmail.com>
2024-08-22 16:16:40 +0300

    Add project files.

---
+++ GameProject/GamerManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameProject
+{
+    //MicroService
+    internal class GamerManager : IGamerService
+    {
+        IUserValidationService _userValidationService;
+
+        public GamerManager(IUserValidationService userValidationService)
+        {
+            _userValidationService = userValidationService;
+        }
+
+        public void Add(Gamer gamer)
+        {
+            if (_userValidationService.Validate(gamer) == true)
+            {
+                Console.WriteLine("User registered successful.");
+            }
+            else
+            {
+                Console.WriteLine("Validation unsuccessful, user registration failed.");
+
+            }
+
+        }
+
+        public void Delete(Gamer gamer)
+        {
+            throw new NotImplementedException("User record deleted.");
+        }
+
+        public void Update(Gamer gamer)
+        {
+            throw new NotImplementedException("User record updated.");
+        }
+    }
+}
---
+++ GameProject/Program.cs
@@ -0,0 +1,11 @@
+﻿using GameProject;
+
+GamerManager gamerManager = new GamerManager(new NewEStateUserValidationManager());
+gamerManager.Add(new Gamer { 
+    ID = 1, BirthYear = 1968, 
+    FirstName = "Dell", 
+    LastName = "Conagher", 
+    IdentityNumber = 12345 
+});
+
+
